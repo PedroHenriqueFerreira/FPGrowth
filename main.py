ac215c91f7efbe0bc9ca47eb8cd4e65aca45c9da
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2023-05-19 18:57:05 -0300

    Database operations

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from fpgrowth import FPGrowth
 
 from utils import one_of, greater_than
 
-transactions = DataBase \
+groups = DataBase \
     .read_csv('SPAECE-MATEMATICA-LISTA-ACERTOS-ERROS-DISTRITO-TODOS.csv', ';') \
     .select_columns(['nm_turma', 'CONJ_DESC_ERRO']) \
-    .filter({ 'nm_turma': one_of('9AAM-98', '9AAT-98') }) \
-    .get_column_data('CONJ_DESC_ERRO', key=lambda i: i.split(','))
+    .filter({ 'nm_turma': one_of('9AAM-98', '9ABM-182') }) \
+    .group_by('nm_turma')
 
-associations = FPGrowth(transactions).get_associations()
-
-print(associations.filter({ 'lift': greater_than(1) }))
+for group in groups:
+    transactions = groups[group].get_column_data('CONJ_DESC_ERRO', key=lambda i: i.split(','))
+    
+    associations = FPGrowth(transactions).get_associations()
+    
+    print(f'\nTurma: {group}\n')
+    
+    print(associations.filter({ 'lift': greater_than(1) }))
